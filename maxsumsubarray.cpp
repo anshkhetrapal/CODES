bd7d429505cee3a6d0b985a48170e655b3c2ad20
anshkhetrapal <113674951+anshkhetrapal@users.noreply.github.com>
2023-05-18 16:33:46 +0530

    Create maxsumsubarray.cpp

---
+++ maxsumsubarray.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    long maximumSumSubarray(int K, vector<int> &Arr , int N){
+        long wstart=0,maxi=0;
+        long  wend=0,sum=0;
+        while(wend<N){
+             sum+=Arr[wend];
+            if(wend-wstart+1==K){
+                maxi=max(maxi,sum);
+                sum-=Arr[wstart];
+                wstart++;
+                
+            }
+            wend++;
+            
+        }
+        return maxi;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N,K;
+        cin >> N >> K;;
+        vector<int>Arr;
+        for(int i=0;i<N;++i){
+            int x;
+            cin>>x;
+            Arr.push_back(x);
+        }
+        Solution ob;
+        cout << ob.maximumSumSubarray(K,Arr,N) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
